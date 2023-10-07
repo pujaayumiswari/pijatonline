9d9aea6b604367315b232778358154661cb88793
pujaayumiswari <payumiswari@gmail.com>
2023-10-08 04:28:04 +0700

    feat: update

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,16 +2,16 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "aws.connect.psdb.cloud",
-    port: 3306,
-    username: "igbw8i274a37mbbnilaa",
-    password: "pscale_pw_dIzNXLpHQ5aP73smgo7qh79zl0EsVjiYOHUB06GTWJi",
-    database: "setsuzu",
-    synchronize: false,
-    logging: true,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"],
-    subscribers: [],
-    ssl: {}
+  type: "mysql",
+  host: "aws.connect.psdb.cloud",
+  port: 3306,
+  username: "5entlv6yjegvydsya1z8",
+  password: "pscale_pw_ua3n4VKB0Sc8HXgqLkISksu1g8imqtYD3YcspHSCzgA",
+  database: "setsuzu",
+  synchronize: false,
+  logging: true,
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+  subscribers: [],
+  ssl: {},
 });
